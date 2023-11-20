39482f8974422caf135be07d3ef50656b7607f1f
chrishormaza <chrishormaza@gmail.com>
2023-11-19 23:49:00 -0500

    added mountain cards

--- scripts/mountainInfo.js
+++ scripts/mountainInfo.js
@@ -19,11 +19,45 @@ function mountainsDropdown(){
     }
 }
 
+async function getSunsetForMountain(lat, lng){
+    let response = await fetch( `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`);
+       let data = await response.json();
+       return data;
+    }
+
 function differentMountain(){
     const mountSelected = document.getElementById("mountainOption");
     let mountainSelected = mountSelected.value;
 
-    for (mountainSelected of mountainsArray){
-        
+    const mountainCard = document.getElementById("mountainCard");
+    mountainCard.style.display = "block";
+
+    const image = document.getElementById("image");
+    const title = document.getElementById("title");
+    const description = document.getElementById("description");
+    const effort = document.getElementById("effort");
+    const elevation = document.getElementById("elevation");
+    const coordsLat = document.getElementById("coordsLat");
+    const coordsLng = document.getElementById("coordsLng");
+
+    for (let mountain of mountainsArray){
+        if (mountainSelected === mountain.name){
+            image.src = `images/mountainImages/${mountain.img}`;
+            title.innerText = mountain.name;
+            description.innerText = mountain.desc;
+            effort.innerText = mountain.effort;
+            elevation.innerText = mountain.elevation;
+            coordsLat.innerText = mountain.coords.lat;
+            coordsLng.innerText = mountain.coords.lng;
+                
+            getSunsetForMountain(mountain.coords.lat, mountain.coords.lng).then((data) => {
+                const sunrise = document.getElementById("sunrise");
+                const sunset = document.getElementById("sunset");
+
+                sunrise.innerText = data.results.sunrise;
+                sunset.innerText = data.results.sunset;
+        });
+
+        }
     }
 }
